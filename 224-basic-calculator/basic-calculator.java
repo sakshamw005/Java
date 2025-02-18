f52a10622abe7c163d8ae1309bbbb7413415da05
sakshamw005 <143003056+sakshamw005@users.noreply.github.com>
2025-02-18 09:19:58 +0530

    Time: 3 ms (94.28%) | Memory: 43.5 MB (91.97%) - LeetSync

---
+++ 224-basic-calculator/basic-calculator.java
@@ -0,0 +1,28 @@
+class Solution {
+    static int i = 0 ;
+    public int calculate(String s) {
+        i = 0 ;
+        return calc(s) ;
+    }
+    public static int calc(String s){
+        int num = 0 , ans = 0 , sign = 1 ;
+        while(i<s.length()){
+            char ch = s.charAt(i++) ;
+            if(Character.isDigit(ch)){
+                num = num*10 + (ch-'0') ;
+            }
+            else if(ch == '('){
+                num = calc(s) ;
+            }
+            else if(ch == ')'){
+                return ans + num * sign ;
+            }
+            else if (ch == '+' || ch == '-') {
+                ans += num * sign;
+                num = 0;
+                sign = ch == '-' ? -1 : 1;
+            }
+        }
+        return ans + num * sign;
+    }
+}
