c54b98055cf5c2d778e372c5a048e1780cc59826
sakshamw005 <143003056+sakshamw005@users.noreply.github.com>
2025-05-30 23:22:15 +0530

    Time: 3 ms (24.62%) | Memory: 44 MB (11.75%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>() ;
+        Set<List<Integer>> temp = new HashSet<>() ;
+        List<Integer> l = new ArrayList<>() ;
+        Arrays.sort(nums) ;
+        print(nums,0,l,temp) ;
+        for(List<Integer> x : temp){
+            ans.add(x) ;
+        }
+        return ans ;
+    }
+    public static void print(int[] nums,int curr ,List<Integer> l ,Set<List<Integer>> temp ){
+        if (curr == nums.length) {
+            temp.add(new ArrayList<>(l)); 
+            return;
+        }
+        l.add(nums[curr]) ;
+        print(nums,curr+1,l,temp) ;
+        l.remove(l.size()-1) ;
+        print(nums, curr + 1, l, temp);
+    }
+}
