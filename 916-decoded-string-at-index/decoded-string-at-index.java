e0455d3416c435f8f53e5bca2e85b9a99bbcd6f1
sakshamw005 <143003056+sakshamw005@users.noreply.github.com>
2025-07-02 21:38:58 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (79.38%) - LeetSync

---
+++ 916-decoded-string-at-index/decoded-string-at-index.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String decodeAtIndex(String s, int k) {
+        long size = 0 ; 
+        for(char c : s.toCharArray()){
+            if(Character.isDigit(c)){
+                size*=c-'0' ;
+            }
+            else{
+                size++ ;
+            }
+        }
+        for(int i = s.length()-1 ; i>=0 ; i--){
+            char c = s.charAt(i);
+            k %= size;
+            if(k==0 && !Character.isDigit(c)){
+                return String.valueOf(c);
+            }
+            if (Character.isDigit(c)) {
+                size /= c - '0';
+            }
+            else{
+                size-- ;
+            }
+        }
+        return "" ;
+    }
+}
