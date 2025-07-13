158a137488892fb654f2b69399d6efbdafd57d56
sakshamw005 <143003056+sakshamw005@users.noreply.github.com>
2025-07-14 01:22:24 +0530

    Time: 6 ms (100.00%) | Memory: 66.2 MB (7.20%) - LeetSync

--- 2497-maximum-matching-of-players-with-trainers/maximum-matching-of-players-with-trainers.java
+++ 2497-maximum-matching-of-players-with-trainers/maximum-matching-of-players-with-trainers.java
@@ -1,18 +1,33 @@
 class Solution {
     public int matchPlayersAndTrainers(int[] players, int[] trainers) {
-        Arrays.sort(players) ;
-        Arrays.sort(trainers) ;
-        int count = 0 , i = 0 , j = 0 ;
-        while(i<players.length && j<trainers.length){
-            if(players[i]<=trainers[j]){
-                i++ ; 
-                j++ ;
-                count++ ;
+        
+        Thread t1 = new Thread(() -> Arrays.sort(players));
+        Thread t2 = new Thread(() -> Arrays.sort(trainers));
+        t1.start();
+        t2.start();
+        try {
+            t1.join();
+            t2.join();
+        } catch (Exception e) {
+        }
+        int count = 0;
+        int tracker = 0;
+        int point = 0;
+
+        while(tracker < trainers.length && point < players.length)
+        {
+            if(players[point] <= trainers[tracker])
+            {
+                count += 1;
+                point += 1;
+                tracker += 1;
             }
-            else{
-                j++ ;
+            else
+            {
+                tracker += 1;
             }
         }
-        return count ;
+
+        return count;
     }
 }
