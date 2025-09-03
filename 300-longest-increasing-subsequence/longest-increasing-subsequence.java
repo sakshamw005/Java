737c59c234ee1ba7603cc058ebdfec8171187ff5
sakshamw005 <143003056+sakshamw005@users.noreply.github.com>
2025-09-03 12:36:13 +0530

    Time: 46 ms (43.14%) | Memory: 44.1 MB (88.90%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,31 +1,15 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        List<Integer> l = new ArrayList<>() ;
-        for(int i = 0 ; i<nums.length ; i++){
-            if(l.isEmpty() || nums[i]>l.get(l.size()-1)){
-                l.add(nums[i]) ;
-            }
-            else{
-                int n = search(l,nums[i]) ;
-                l.set(n,nums[i]) ;
-            }
-        }
-        return l.size() ;
-    }
-    public int search(List<Integer> l , int target){
-        int i = 0 , j = l.size()-1 ;
-        while(i<=j){
-            int mid = (i+j)/2 ;
-            if(l.get(mid)==target){
-                return mid ;
-            }
-            if(l.get(mid)>target){
-                j = mid - 1 ; ;
-            }
-            else{
-                i = mid+1 ;
+        int[] dp = new int[nums.length] ;
+        Arrays.fill(dp,1);
+        for(int i = 1 ; i<dp.length ; i++){
+            for(int j = i-1 ; j >= 0 ; j--){
+                if(nums[i]>nums[j]){
+                    int x = dp[j];
+                    dp[i] = Math.max(dp[i],x+1);
+                }
             }
         }
-        return i ;
+        return Arrays.stream(dp).max().getAsInt();
     }
 }
