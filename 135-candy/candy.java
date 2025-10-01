0df2a1b5a376fedf3cdcf0b658295c0f5dc9d51d
sakshamw005 <143003056+sakshamw005@users.noreply.github.com>
2025-10-01 11:12:16 +0530

    Time: 3 ms (80.30%) | Memory: 45.7 MB (71.29%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -1,24 +1,35 @@
 class Solution {
     public int candy(int[] ratings) {
-        int n = ratings.length ;
-        int[] candies = new int[n] ;
-        int cnt = 0 ;
-        for(int i = 0 ; i<n ; i++){
-            candies[i] = 1 ;
-        }
-        //Left se
-        for(int i = 1 ; i<n ; i++){
-            if(ratings[i]>ratings[i-1]){
-                candies[i] = candies[i-1]+1 ;
+        int[] ans = new int[ratings.length] ;
+        int curr = 1 ;
+        for(int i = 0 ; i < ratings.length ; i++){
+            if(i==0)ans[i] = curr ;
+            else{
+                if (ratings[i] > ratings[i - 1]) {
+                    curr++;
+                } else {
+                    curr = 1; 
+                }
+                ans[i] = curr;
             }
         }
-        //Right se
-        for(int i = n-1 ; i>0 ; i--){
-            if (ratings[i - 1] > ratings[i]){
-                candies[i - 1] = Math.max(candies[i] + 1, candies[i - 1]);
+        curr = 1 ;
+        for(int i = ratings.length-1 ; i>=0 ; i--){
+            if(i==ratings.length-1)ans[i] = Math.max(ans[i],curr) ;
+            else{
+                if(ratings[i+1]<ratings[i]){
+                    curr++ ;
+                }
+                else{
+                    curr = 1 ;
+                }
+                ans[i] = Math.max(ans[i] , curr) ;
             }
-            cnt += candies[i - 1]; 
         }
-        return cnt + candies[n - 1]; 
+        int res = 0 ;
+        for(int i : ans){
+            res+=i ;
+        }
+        return res ;
     }
 }
