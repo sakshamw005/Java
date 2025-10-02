46212ddd98266175131505cb5b6bea9e97b40db0
sakshamw005 <143003056+sakshamw005@users.noreply.github.com>
2025-10-02 22:11:36 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (25.81%) - LeetSync

--- 3336-water-bottles-ii/water-bottles-ii.java
+++ 3336-water-bottles-ii/water-bottles-ii.java
@@ -3,15 +3,11 @@ class Solution {
         if(n<ex)return n ;
         int ans = n ;
         int em = n ;
-        n = 0 ;
         while(em>=ex){
             em-=ex ;
-            n++ ;
             ex++ ;
-            if(n>0){
-                ans+= n-- ;
-                em++ ;
-            }
+            ans++ ;
+            em++ ;
         }
         return ans ;
     }
