a36fd531cbabad61dfc50d5705ac15f42e180128
sakshamw005 <143003056+sakshamw005@users.noreply.github.com>
2025-10-07 07:56:06 +0530

    Time: 2062 ms (5.00%) | Memory: 53.7 MB (7.10%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,20 +1,21 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> l = new ArrayList<>();
-        Set<List<Integer>> set = new HashSet<>();
-        for (int i = 0; i < nums.length; i++) {
-            HashMap<Integer,Integer> m = new HashMap<>() ;
-            for (int j = i + 1; j < nums.length; j++) {
-                int temp = -(nums[i]+nums[j]) ;
-                if(m.containsKey(temp)){
-                    ArrayList<Integer> x = new ArrayList<>(Arrays.asList(nums[i],nums[j],temp)) ;
-                    Collections.sort(x) ;
-                    set.add(x) ;
+        List<List<Integer>> ans = new ArrayList<>() ;
+        Set<List<Integer>> s = new HashSet<>() ;
+
+        for(int i = 0 ; i < nums.length ; i++){
+            Map<Integer,Integer> map = new HashMap<>() ;
+            for(int j = i+1 ; j<nums.length ; j++){
+                int key = -(nums[i]+nums[j]) ;
+                if(map.containsKey(key)){
+                    List<Integer> ll = new ArrayList<>(Arrays.asList(nums[i],nums[j],key)) ;
+                    Collections.sort(ll) ;
+                    s.add(ll) ;
                 }
-                m.put(nums[j],j) ;
+                map.put(nums[j],j) ;
             }
         }
-        l.addAll(set) ;
-        return l ;
+        ans.addAll(s) ;
+        return ans ;
     }
 }
