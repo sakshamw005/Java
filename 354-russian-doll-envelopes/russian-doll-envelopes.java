5e1aeb487e77cd3894e06e0f1ae63b72a991f097
sakshamw005 <143003056+sakshamw005@users.noreply.github.com>
2025-10-08 07:42:05 +0530

    Time: 37 ms (97.20%) | Memory: 104.6 MB (24.28%) - LeetSync

--- 354-russian-doll-envelopes/russian-doll-envelopes.java
+++ 354-russian-doll-envelopes/russian-doll-envelopes.java
@@ -8,17 +8,18 @@ class Solution {
     }
     public int LIS(int[][] arr){
         int[] nums = new int[arr.length] ;
-        int len = 1;
-        nums[0] = arr[0][1];
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i][1] > nums[len - 1]) {
-                nums[len++] = arr[i][1];
-            } else {
-                int idx = BinarySearch(arr[i][1], nums, len, 0);
-                nums[idx] = arr[i][1];
+        int len = 1 ;
+        nums[0] = arr[0][1] ;
+        for(int i = 1 ; i<arr.length ; i++){
+            if(arr[i][1] > nums[len-1]){
+                nums[len++] = arr[i][1] ;
+            }
+            else{
+                int idx = BinarySearch(arr[i][1] , nums , len , 0 ) ;
+                nums[idx] = arr[i][1] ;
             }
         }
-        return len;
+        return len ;
     }
     public int BinarySearch(int ele , int[] nums , int high , int low){
         while(low<high){
