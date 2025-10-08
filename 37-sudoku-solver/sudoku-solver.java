ee9adebf18d42db6f9fb4997933dab33a69ec25e
sakshamw005 <143003056+sakshamw005@users.noreply.github.com>
2025-10-08 11:08:22 +0530

    Time: 157 ms (72.34%) | Memory: 41.5 MB (26.27%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.java
+++ 37-sudoku-solver/sudoku-solver.java
@@ -1,9 +1,8 @@
 class Solution {
     public void solveSudoku(char[][] board) {
-        print(board,0,0) ;
-        printBoard(board);
+        solve(board,0,0) ;
     }
-    public static boolean print(char[][] board , int row , int col){
+    public boolean solve(char[][] board,int row,int col){
         if(col==9){
             row++ ;
             col = 0 ;
@@ -11,49 +10,43 @@ class Solution {
         if(row==9){
             return true ;
         }
-        if (board[row][col] != '.') {
-            return print(board, row, col + 1);
-        } else {
-            for (char val = '1' ; val <= '9' ; val++) {
-                if (isItSafe(board, row, col, val)) {
-                    board[row][col] = val;
-                    if (print(board, row, col + 1)) {
-                        return true;
-                    }
-                    board[row][col] = '.';
+        if(board[row][col]!='.'){
+            return solve(board,row,col+1) ;
+        }
+        for(char x = '1' ; x<='9' ; x++){
+            if(safe(board,row,col,x)){
+                board[row][col] = x ;
+                if(solve(board,row,col+1)){
+                    return true ;
+                }
+                board[row][col] = '.' ;
+            }
+        }
+        return false ;
+    }
+    public boolean safe(char board[][] , int row , int col , char ch){
+        //row
+        for(int i = 0 ; i<board.length ; i++){
+            if(board[row][i]==ch){
+                return false ;
+            }
+        }
+        //col
+        for(int i = 0 ; i<board.length ; i++){
+            if(board[i][col]==ch){
+                return false ;
+            }
+        }
+        //3x3 sub-matrix
+        int r = row - row%3 ;
+        int c = col - col%3 ;
+        for(int i = r ; i<r+3 ; i++){
+            for(int j = c ; j<c+3 ; j++){
+                if(board[i][j]==ch){
+                    return false ;
                 }
             }
         }
-        return false;
-    }
-    public static void printBoard(char[][] board) {
-        for (char[] row : board) {
-            System.out.println(Arrays.toString(row));
-        }
-    }
-    public static boolean isItSafe(char[][] grid, int row, int col,char val) {
-        // Check row
-        for (int i = 0; i < grid.length; i++) {
-            if (grid[row][i] == val) {
-                return false;
-            }
-        }
-        // Check column
-        for (int i = 0; i < grid.length; i++) {
-            if (grid[i][col] == val) {
-                return false;
-            }
-        }
-        // Check 3x3 sub-grid
-        int r = row - row % 3;
-        int c = col - col % 3;
-        for (int i = r; i < r + 3; i++) {
-            for (int j = c; j < c + 3; j++) {
-                if (grid[i][j] == val) {
-                    return false;
-                }
-            }
-        }
-        return true;
+        return true ;
     }
 }
