066099a0c03d54ccacc7b6eed15fec1fae58b994
Saksham Wadhwa <143003056+sakshamw005@users.noreply.github.com>
2025-12-18 01:11:22 +0530

    Time: 3 ms (2.30%) | Memory: 44.3 MB (9.34%) - LeetSync

---
+++ 78-subsets/subsets.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>() ;
+        Arrays.sort(nums) ;
+        print(nums,0,ans,new ArrayList<Integer>()) ;
+        return ans ;
+    }
+    public void print(int[] nums , int i , List<List<Integer>> ans , List<Integer> l){
+        if(i==nums.length){
+            ans.add(new ArrayList<>(l)) ;
+            return ;
+        }
+        print(nums,i+1,ans,l) ;
+        l.add(nums[i]) ;
+        print(nums,i+1,ans,l) ;
+        l.remove(l.size()-1) ;
+    }
+}
