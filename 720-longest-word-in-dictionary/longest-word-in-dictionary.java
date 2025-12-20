7130a03b5d6cce9397247fad8f422f83a387d01a
Saksham Wadhwa <143003056+sakshamw005@users.noreply.github.com>
2025-12-20 16:15:03 +0530

    Time: 18 ms (29.93%) | Memory: 46.4 MB (66.04%) - LeetSync

---
+++ 720-longest-word-in-dictionary/longest-word-in-dictionary.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String longestWord(String[] words) {
+        Arrays.sort(words); 
+        
+        Set<String> built = new HashSet<>();
+        String ans = "";
+        
+        for (String word : words) {
+            if (word.length() == 1 || built.contains(word.substring(0, word.length() - 1))) {
+                built.add(word);
+                if (word.length() > ans.length()) {
+                    ans = word;
+                }
+            }
+        }
+        return ans;
+    }
+}
