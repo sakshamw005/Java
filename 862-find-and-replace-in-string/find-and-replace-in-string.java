cc144960aff7204c95811e6e839664d90e2934f9
Saksham Wadhwa <143003056+sakshamw005@users.noreply.github.com>
2025-12-22 13:52:15 +0530

    Time: 2 ms (76.01%) | Memory: 44.1 MB (17.63%) - LeetSync

--- 862-find-and-replace-in-string/find-and-replace-in-string.java
+++ 862-find-and-replace-in-string/find-and-replace-in-string.java
@@ -1,22 +1,21 @@
 class Solution {
     public String findReplaceString(String s, int[] indices, String[] sources, String[] targets) {
-        Map<Integer,Integer> m = new HashMap<>() ;
-        for(int i = 0 ; i < indices.length ; i++){
-            if(s.startsWith(sources[i] , indices[i])){
-                m.put(indices[i],i) ;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < sources.length; i++) {
+            if (s.startsWith(sources[i], indices[i])) {
+                map.put(indices[i], i);
             }
         }
-        StringBuilder ans = new StringBuilder() ;
-        for(int i = 0 ; i<s.length() ;){
-            if(!m.containsKey(i)){
-                ans.append(s.charAt(i)) ;
-                i +=1 ;
-            }
-            else{
-                ans.append(targets[m.get(i)]) ;
-                i = i + sources[m.get(i)].length() ;
+        StringBuilder ans = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (map.containsKey(i)) {
+                int idx = map.get(i);
+                ans.append(targets[idx]);
+                i += sources[idx].length() - 1;  
+            } else {
+                ans.append(s.charAt(i));
             }
         }
-        return ans.toString() ;
+        return ans.toString();
     }
 }
