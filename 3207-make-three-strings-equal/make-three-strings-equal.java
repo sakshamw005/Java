d3a775f4bd520b4c5ba88d12d05f52c6cfce5ee1
Saksham Wadhwa <143003056+sakshamw005@users.noreply.github.com>
2025-12-24 11:50:45 +0530

    Time: 2 ms (21.76%) | Memory: 46.7 MB (18.06%) - LeetSync

---
+++ 3207-make-three-strings-equal/make-three-strings-equal.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findMinimumOperations(String x, String y, String z) {
+        if (x.equals(y) && y.equals(z))
+            return 0 ;
+        int i = 0 ;
+        while(i<x.length() && i<y.length() && i<z.length()){
+            if(x.charAt(i)==y.charAt(i) && y.charAt(i)==z.charAt(i)){
+                i++ ;
+            }
+            else{
+                break ;
+            }
+        }
+        return i==0 ? -1 : (x.length() - i) + (y.length() - i) + (z.length() - i);
+    }
+}
