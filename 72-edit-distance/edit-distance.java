21243b53c9e173ed6ddb106ea62e597a591f7e6d
Saksham Wadhwa <143003056+sakshamw005@users.noreply.github.com>
2026-01-04 11:45:47 +0530

    Time: 2 ms (99.61%) | Memory: 47.1 MB (52.38%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -6,18 +6,9 @@ class Solution {
     public int find(String s, String t,int i , int j ,Integer dp[][]){
         if(j<0)return i+1 ;
         if(i<0)return j+1 ;
-
         if (dp[i][j] != null) return dp[i][j];
-
-        if(s.charAt(i)==t.charAt(j)){ //equal
-            dp[i][j] = find(s,t,i-1,j-1,dp) ;
-        }//adding
-        else{
-            int add = find(s,t,i,j-1,dp) ; //i pointer remains at same pos , j moves 
-            int del = find(s,t,i-1,j,dp) ; //
-            int rep = find(s,t,i-1,j-1,dp) ;
-            dp[i][j] = 1 + Math.min(add,Math.min(del,rep)) ;
-        }
+        if(s.charAt(i)==t.charAt(j))dp[i][j] = find(s,t,i-1,j-1,dp) ; //adding
+        else dp[i][j] = 1 + Math.min(find(s,t,i,j-1,dp),Math.min(find(s,t,i-1,j,dp),find(s,t,i-1,j-1,dp) )) ;
         return dp[i][j] ;
     }
 }
