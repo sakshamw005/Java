aada1cd53accb38fd1cdf7422151022f90a9ab09
Saksham Wadhwa <143003056+sakshamw005@users.noreply.github.com>
2026-01-04 11:41:27 +0530

    Time: 3 ms (96.54%) | Memory: 47.2 MB (52.38%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,24 +1,23 @@
 class Solution {
-    public int minDistance(String s, String t) {
-        Integer dp[][] = new Integer[s.length()][t.length()] ;
-        return edit(s,t,0,0,dp) ;
+    public int minDistance(String word1, String word2) {
+        Integer dp[][] = new Integer[word1.length()][word2.length()] ;
+        return find(word1,word2,word1.length()-1,word2.length()-1,dp) ;
     }
-    public int edit(String s , String t , int i , int j,Integer dp[][]){
-        if(i==s.length()){
-            return t.length()-j ;
+    public int find(String s, String t,int i , int j ,Integer dp[][]){
+        if(j<0)return i+1 ;
+        if(i<0)return j+1 ;
+
+        if (dp[i][j] != null) return dp[i][j];
+
+        if(s.charAt(i)==t.charAt(j)){ //equal
+            dp[i][j] = find(s,t,i-1,j-1,dp) ;
+        }//adding
+        else{
+            int add = find(s,t,i,j-1,dp) ; //i pointer remains at same pos , j moves 
+            int del = find(s,t,i-1,j,dp) ; //
+            int rep = find(s,t,i-1,j-1,dp) ;
+            dp[i][j] = 1 + Math.min(add,Math.min(del,rep)) ;
         }
-        if(j==t.length()){
-            return s.length()-i ;
-        }
-        if(dp[i][j]!=null){
-            return dp[i][j] ;
-        }
-        if(s.charAt(i)==t.charAt(j)){
-            return dp[i][j] =  edit(s,t,i+1,j+1,dp) ;
-        }
-        int in = 1 + edit(s,t,i,j+1,dp) ;
-        int de = 1 + edit(s,t,i+1,j,dp) ;
-        int re = 1 + edit(s,t,i+1,j+1,dp) ;
-        return dp[i][j] = Math.min(in,Math.min(de,re)) ;
+        return dp[i][j] ;
     }
 }
