01aa145ad242e9d971699e424fa0c5e4e9dcfa20
Saksham Wadhwa <143003056+sakshamw005@users.noreply.github.com>
2026-01-05 15:43:43 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (33.28%) - LeetSync

--- 1022-unique-paths-iii/unique-paths-iii.java
+++ 1022-unique-paths-iii/unique-paths-iii.java
@@ -1,32 +1,28 @@
 class Solution {
     public int uniquePathsIII(int[][] grid) {
-        int x = 0 , r = 0 , c = 0 ;
+        int zeroes = 0 , r = 0 , c = 0 ;
         for(int i = 0 ; i<grid.length ; i++){
             for(int j = 0 ; j<grid[0].length ; j++){
-                if(grid[i][j]==0)x++ ;
+                if(grid[i][j]==0)zeroes++ ;
                 else if(grid[i][j]==1){
-                    r = i ; 
+                    r = i ;
                     c = j ;
                 }
             }
-        } 
-        return call(r,c,grid,x) ;
+        }
+        return print(grid,r,c,zeroes) ;
     }
-    public int call(int r , int c , int[][] grid,int x){
-        if(r>=grid.length || r<0 || c>=grid[0].length || c<0 || grid[r][c]==-1){
+    public int print(int[][] grid , int r , int c , int x){
+        if(r<0 || r>=grid.length || c<0 || c>=grid[0].length || grid[r][c]==-1){
             return 0 ;
         }
-        if(grid[r][c]==2){
-            return x == -1?1:0 ;
-        }
+        if(grid[r][c]==2)return x==-1?1:0 ;
         grid[r][c] = -1 ;
         x-- ;
-
-        int tot = call(r+1,c,grid,x) + call(r,c-1,grid,x) + call(r-1,c,grid,x) + call(r,c+1,grid,x) ;
-        
+        int tot = print(grid,r+1,c,x) + print(grid,r-1,c,x) + 
+                  print(grid,r,c+1,x) + print(grid,r,c-1,x) ;
         grid[r][c] = 0 ;
         x++ ;
-
         return tot ;
     }
 }
