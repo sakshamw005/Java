ad3ee7711bb9367b07b970339140f6b9bda3be0d
Saksham Wadhwa <143003056+sakshamw005@users.noreply.github.com>
2026-01-05 12:51:24 +0530

    Time: 7 ms (47.88%) | Memory: 68.5 MB (6.67%) - LeetSync

--- 2089-maximum-matrix-sum/maximum-matrix-sum.java
+++ 2089-maximum-matrix-sum/maximum-matrix-sum.java
@@ -19,12 +19,10 @@ class Solution {
             int min = Integer.MAX_VALUE ;
             for(int i = 0 ; i<matrix.length ; i++){
                 for(int j = 0 ;j<matrix[0].length ; j++){
-                    
-                        min = Math.min(min,Math.abs(matrix[i][j])) ;
-                    
+                    min = Math.min(min,Math.abs(matrix[i][j])) ;
                 }
             }
-            System.out.println(min) ;
+            // System.out.println(min) ;
             return tot - 2*min ;
         }
 
