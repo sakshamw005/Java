32a62fdda1daf2de2eb7722c3f08562585ec1df6
Saksham Wadhwa <143003056+sakshamw005@users.noreply.github.com>
2026-01-05 12:52:44 +0530

    Time: 8 ms (19.39%) | Memory: 68.1 MB (34.55%) - LeetSync

--- 2089-maximum-matrix-sum/maximum-matrix-sum.java
+++ 2089-maximum-matrix-sum/maximum-matrix-sum.java
@@ -1,27 +1,19 @@
 class Solution {
     public long maxMatrixSum(int[][] matrix) {
         int c = 0 ;
+        long tot = 0L ;
+        int min = Integer.MAX_VALUE ;
         for(int i = 0 ; i<matrix.length ; i++){
             for(int j = 0 ;j<matrix[0].length ; j++){
                 if(matrix[i][j]<0)c++ ;
-            }
-        }
-        long tot = 0 ;
-        for(int i = 0 ; i<matrix.length ; i++){
-            for(int j = 0 ;j<matrix[0].length ; j++){
                 tot+=Math.abs(matrix[i][j]) ;
+                min = Math.min(min,Math.abs(matrix[i][j])) ;
             }
         }
         if(c%2==0){
             return tot ;
         }
         else{
-            int min = Integer.MAX_VALUE ;
-            for(int i = 0 ; i<matrix.length ; i++){
-                for(int j = 0 ;j<matrix[0].length ; j++){
-                    min = Math.min(min,Math.abs(matrix[i][j])) ;
-                }
-            }
             // System.out.println(min) ;
             return tot - 2*min ;
         }
