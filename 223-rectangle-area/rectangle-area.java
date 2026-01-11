5c49520bf3a61f719ca96c9758f80c0e9560f530
Saksham Wadhwa <143003056+sakshamw005@users.noreply.github.com>
2026-01-11 18:17:12 +0530

    Time: 1 ms (100.00%) | Memory: 43.9 MB (97.64%) - LeetSync

---
+++ 223-rectangle-area/rectangle-area.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
+        // int A = (ax2-ax1)*(ay2-ay1) , B = (bx2-bx1)*(by2-by1) ;
+        // int ovl = Math.max(0, Math.min(ax2, bx2) - Math.max(ax1, bx1));
+        // int ovb = Math.max(0, Math.min(ay2, by2) - Math.max(ay1, by1));
+        return (ax2-ax1)*(ay2-ay1)+(bx2-bx1)*(by2-by1) -
+        Math.max(0, Math.min(ax2, bx2) - Math.max(ax1, bx1))*
+        Math.max(0, Math.min(ay2, by2) - Math.max(ay1, by1));
+    }
+}
