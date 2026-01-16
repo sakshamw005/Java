ceeb149549b6b3cf7677a19a62446434f3aafe34
Saksham Wadhwa <143003056+sakshamw005@users.noreply.github.com>
2026-01-16 09:04:41 +0530

    Time: 1 ms (100.00%) | Memory: 79.5 MB (85.00%) - LeetSync

--- 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.java
+++ 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.java
@@ -2,14 +2,15 @@ class Solution {
     public int minOperations(int[] nums, int k) {
         int xor = 0 ;
         for(int i : nums)xor^=i ;
-        String x = Integer.toBinaryString(xor) , y = Integer.toBinaryString(k) ;
-        int max = Math.max(x.length(),y.length()) ;
-        x = String.format("%" + max + "s", x).replace(' ', '0');
-        y = String.format("%" + max + "s", y).replace(' ', '0');
-        int ans = 0;
-        for(int i = 0 ; i<x.length() ; i++){
-            if(x.charAt(i)!=y.charAt(i))ans++ ;
-        }
-        return ans ;
+        // String x = Integer.toBinaryString(xor) , y = Integer.toBinaryString(k) ;
+        // int max = Math.max(x.length(),y.length()) ;
+        // x = String.format("%" + max + "s", x).replace(' ', '0');
+        // y = String.format("%" + max + "s", y).replace(' ', '0');
+        // int ans = 0;
+        // for(int i = 0 ; i<x.length() ; i++){
+        //     if(x.charAt(i)!=y.charAt(i))ans++ ;
+        // }
+        xor^=k ;
+        return Integer.bitCount(xor) ;
     }
 }
