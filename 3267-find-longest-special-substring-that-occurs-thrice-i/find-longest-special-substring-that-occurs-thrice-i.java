1e381d3fffed235b184a0021bfd90b5875b60af1
Saksham Wadhwa <143003056+sakshamw005@users.noreply.github.com>
2026-01-20 23:26:16 +0530

    Time: 4 ms (72.93%) | Memory: 46.6 MB (56.35%) - LeetSync

---
+++ 3267-find-longest-special-substring-that-occurs-thrice-i/find-longest-special-substring-that-occurs-thrice-i.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int maximumLength(String s) {
+        int n = s.length() ;
+        int[][] freq = new int[26][n+1] ; //[char][length] = count of occurence
+        int x = 1 ;
+        for(int i = 0 ; i<n ; i++){
+            if(i>0 && s.charAt(i)==s.charAt(i-1)){
+                x++ ;
+            }
+            else{
+                x = 1 ;
+            }
+            freq[s.charAt(i)-'a'][x]++ ; 
+        }
+        // for(int i = 0 ; i<26 ; i++){
+        //     for(int j = 0 ; j<n ; j++){
+        //         System.out.print(freq[i][j]) ;
+        //     }
+        //     System.out.println("*") ;
+        // }
+        int ans = -1 ;
+        for(int i = 0 ; i<26 ; i++){
+            for(int j = n ; j>=1 ; j--){
+                if(j<n)freq[i][j] += freq[i][j+1] ;
+                if(freq[i][j]>=3){
+                    ans = Math.max(ans,j) ;
+                    break ;
+                }
+            }
+        }
+        return ans ;
+    }
+}
