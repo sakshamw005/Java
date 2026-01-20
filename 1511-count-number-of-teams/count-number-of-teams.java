98a92ac628e30e8fc6d23b59b05de97cabe0d40f
Saksham Wadhwa <143003056+sakshamw005@users.noreply.github.com>
2026-01-20 15:27:15 +0530

    Time: 2749 ms (5.06%) | Memory: 44.2 MB (52.87%) - LeetSync

---
+++ 1511-count-number-of-teams/count-number-of-teams.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numTeams(int[] rating) {
+        int count = 0 ;
+        int n = rating.length ;
+        for(int i = 0 ; i<n ; i++){
+            for(int j = i ; j<n ; j++){
+                for(int k = j ; k<n ;k++){
+                    if((rating[i]<rating[j])&&(rating[j]<rating[k])||(rating[i]>rating[j])&&(rating[j]>rating[k])){
+                        count++ ;
+                    }
+                }
+            }
+        }
+        return count; 
+    }
+}
