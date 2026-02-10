33779f717a5b8ae3930802ca9c00e5e9fc36fd40
Saksham Wadhwa <143003056+sakshamw005@users.noreply.github.com>
2026-02-10 11:55:05 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (79.17%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -1,28 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        return ValidBST(root).isbst;
+        return check(root,Long.MIN_VALUE,Long.MAX_VALUE) ;
     }
-
-    public BstPair ValidBST(TreeNode root) {
-        if (root == null) {
-            return new BstPair(); 
-        }
-
-        BstPair lbp = ValidBST(root.left);
-        BstPair rbp = ValidBST(root.right);
-
-        BstPair sbp = new BstPair();
-        sbp.min = Math.min(root.val, Math.min(lbp.min, rbp.min));
-        sbp.max = Math.max(root.val, Math.max(lbp.max, rbp.max));
-
-        sbp.isbst = lbp.isbst && rbp.isbst && lbp.max < root.val && rbp.min > root.val;
-
-        return sbp;
-    }
-
-    class BstPair {
-        boolean isbst = true;
-        long max = Long.MIN_VALUE;
-        long min = Long.MAX_VALUE;
+    public boolean check(TreeNode root , long left , long right){
+        if(root==null)return true ;
+        if (root.val <= left || root.val >= right) return false;
+        return check(root.left, left, root.val) && check(root.right, root.val, right);
     }
 }
