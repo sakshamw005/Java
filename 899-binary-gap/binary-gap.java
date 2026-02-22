7edade156d080f684df3ccf0bb06a2565cd7a003
Saksham Wadhwa <143003056+sakshamw005@users.noreply.github.com>
2026-02-22 08:48:44 +0530

    Time: 1 ms (40.87%) | Memory: 42.5 MB (30.35%) - LeetSync

---
+++ 899-binary-gap/binary-gap.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int binaryGap(int n) {
+        String s = Integer.toBinaryString(n) ;
+        int ans = 0 ; 
+        for(int i = 0 ; i<s.length() ;i++){
+            if(s.charAt(i)=='1'){
+                for(int j = i+1 ; j<s.length() ; j++){
+                    if(s.charAt(j)=='1'){
+                        ans = Math.max(j-i,ans) ;
+                        i = j - 1 ;
+                        break;
+                    }
+                }
+            }
+        }
+        return ans ;
+    }
+}
