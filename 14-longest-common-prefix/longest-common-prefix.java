3029a3c18279d5953fc588f37e8bcd751679dffc
Saksham Wadhwa <143003056+sakshamw005@users.noreply.github.com>
2026-03-12 00:09:53 +0530

    Time: 1 ms (65.04%) | Memory: 43.6 MB (8.00%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,16 +1,47 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        Arrays.sort(strs) ;
-        String x = strs[0] , y = strs[strs.length-1] ;
-        int i = 0 ;
-        while(i<x.length() && i<y.length()){
-            if(x.charAt(i)==y.charAt(i)){
-                i++ ;
-            }
-            else{
-                break ;
-            }
+        Trie trie = new Trie() ;
+        for(String i : strs){
+            trie.insert(i) ;
         }
-        return x.substring(0,i) ;
+        return trie.getLCP(strs[0]) ;
+    }
+    class TrieNode{
+        TrieNode[] child = new TrieNode[26] ;
+        boolean isEnd ;
+    }
+    class Trie{
+        TrieNode root ;
+        public Trie(){
+            root = new TrieNode() ;
+        }
+        void insert(String w){
+            TrieNode curr = root ;
+            for(char x : w.toCharArray()){
+                if(curr.child[x-'a']==null){
+                    curr.child[x-'a'] = new TrieNode() ;
+                }
+                curr = curr.child[x-'a'] ;
+            }
+            curr.isEnd = true ;
+        }
+        String getLCP(String word){
+            TrieNode curr = root ;
+            StringBuilder ans = new StringBuilder() ;
+            for(char x : word.toCharArray()){
+                int c = 0 ;
+                for(int i = 0 ; i<26 ; i++){
+                    if(curr.child[i]!=null)c++ ;
+                }
+                if(c==1 && !curr.isEnd ){
+                    ans.append(x) ;
+                    curr = curr.child[x-'a'] ;
+                }
+                else{
+                    break ;
+                }
+            }
+            return ans.toString() ;
+        }
     }
 }
