586474272fb0c660451a81a01607304f01bca39b
Saksham Wadhwa <143003056+sakshamw005@users.noreply.github.com>
2026-03-17 10:52:47 +0530

    Time: 13 ms (86.96%) | Memory: 113.8 MB (75.65%) - LeetSync

--- 1845-largest-submatrix-with-rearrangements/largest-submatrix-with-rearrangements.java
+++ 1845-largest-submatrix-with-rearrangements/largest-submatrix-with-rearrangements.java
@@ -1,28 +1,18 @@
 class Solution {
     public int largestSubmatrix(int[][] matrix) {
-        int m = matrix.length , n = matrix[0].length ;
-        int[][] col = new int[matrix.length][matrix[0].length] ;
-        for(int j = 0 ; j < n ; j++) col[0][j] = matrix[0][j];
-        for(int i = 1 ; i<matrix.length ; i++){
-           for(int j = 0 ; j<matrix[0].length ; j++){
-                if(matrix[i][j] == 1) col[i][j] = col[i-1][j] + 1;
-                else col[i][j] = 0;
+        int m = matrix.length, n = matrix[0].length, res = 0;
+        for (int i = 1; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (matrix[i][j] == 1) matrix[i][j] = matrix[i - 1][j] + 1;
             }
         }
-        // for(int i = 0 ; i<matrix.length ; i++){
-        // for(int j = 0 ; j<matrix[0].length ; j++){
-        //     System.out.print(col[i][j]+" ") ;
-        // }
-        // System.out.println() ;
-        // } 
-        int ans = -1 ;
-        for(int i = 0 ; i<m ; i++){
-            int[] curr = col[i] ;
-            Arrays.sort(curr) ;
-            for(int j = 0 ; j<curr.length ; j++){
-                ans = Math.max(ans , curr[j]*(curr.length-j)) ;
+
+        for (int i = 0; i < m; ++i) {
+            Arrays.sort(matrix[i]);
+            for (int j = 1; j <= n; ++j) {
+                res = Math.max(res, j * matrix[i][n - j]);
             }
         }
-        return ans ;
+        return res;
     }
 }
