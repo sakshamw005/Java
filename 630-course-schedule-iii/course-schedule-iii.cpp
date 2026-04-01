57de767f006f23d6e9988f4aa0f0a30458d34ab8
Saksham Wadhwa <143003056+sakshamw005@users.noreply.github.com>
2026-04-01 16:05:24 +0530

    Time: 650 ms (10.43%) | Memory: 60.4 MB (74.81%) - LeetSync

---
+++ 630-course-schedule-iii/course-schedule-iii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int scheduleCourse(vector<vector<int>>& courses) {
+        sort(courses.begin(), courses.end(), [](vector<int>& a, vector<int>& b){
+            return a[1] < b[1];
+        });
+        for(int i = 0 ; i<courses.size() ; i++){
+            cout << courses[i][0] << " " << courses[i][1] << '\n' ;
+        }
+        priority_queue<int> pq ;
+        int cur = 0 ;
+        for(int i = 0 ; i<courses.size() ; i++){
+            cur += courses[i][0] ;
+            pq.push(courses[i][0]) ;
+            if(cur > courses[i][1]){
+                cur -= pq.top() ;
+                pq.pop() ;
+            }
+        }
+        return pq.size() ;
+    }
+};
