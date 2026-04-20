e97180da58868aa780421094639ae95b1447e488
Saksham Wadhwa <143003056+sakshamw005@users.noreply.github.com>
2026-04-21 00:28:59 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (57.48%) - LeetSync

---
+++ 413-arithmetic-slices/arithmetic-slices.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int numberOfArithmeticSlices(vector<int>& nums) {
+        int ans = 0;
+        int curr = 0;
+        for(int i = 2; i < nums.size(); i++){
+            if(nums[i] - nums[i-1] == nums[i-1] - nums[i-2]){
+                curr += 1;
+                ans += curr;
+            } 
+            else {
+                curr = 0;
+            }
+        }
+        return ans;
+    }
+};
