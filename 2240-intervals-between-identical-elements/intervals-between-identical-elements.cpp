53a1f8394dc7d770a401cd5e58886dc880ec00b8
Saksham Wadhwa <143003056+sakshamw005@users.noreply.github.com>
2026-04-23 15:20:58 +0530

    Time: 99 ms (82.33%) | Memory: 129.4 MB (79.86%) - LeetSync

---
+++ 2240-intervals-between-identical-elements/intervals-between-identical-elements.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<long long> getDistances(vector<int>& nums) {
+        unordered_map<int, vector<int>> ma;
+        for (int i = 0; i < nums.size(); i++) {
+            ma[nums[i]].push_back(i);
+        }
+        vector<long long> ans(nums.size(), 0);
+        for (auto &it : ma) {
+            vector<int> &pos = it.second;
+            int n = pos.size();
+            if (n == 1) continue;
+            vector<long long> prefix(n + 1, 0);
+            for (int i = 0; i < n; i++) {
+                prefix[i + 1] = prefix[i] + pos[i];
+            }
+            for (int i = 0; i < n; i++) {
+                long long left = (long long)pos[i] * i - prefix[i];
+                long long right = (prefix[n] - prefix[i + 1]) - (long long)pos[i] * (n - i - 1);
+                ans[pos[i]] = left + right;
+            }
+        }
+
+        return ans;
+    }
+};
